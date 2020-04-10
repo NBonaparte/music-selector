f07d02a1b34b7b133abfc7dd82c8cfbec816e677
NBonaparte <nbonaparte@protonmail.com>
2020-04-10 14:21:54 -0700

    feat: use sliders

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-var tiers = [];
+var works = [];
+var dates = document.getElementById("date-range");
+noUiSlider.create(dates, {
+  start: [1000, 2020],
+  connect: true,
+  step: 20,
+  range: {min: 1000, max: 2020},
+  pips: {mode: "values", values: [...Array(11).keys()].map(x => (x + 10) * 100), density: 2},
+});
+
+var tier_pips = [...Array(11).keys()].map(x => (x + 1) * 10);
+tier_pips.unshift(1);
+tier_pips.push(108);
+var tiers = document.getElementById("tier-range");
+noUiSlider.create(tiers, {
+  start: [1, 108],
+  connect: true,
+  step: 1,
+  range: {min: 1, max: 108},
+  pips: {mode: "values", values: tier_pips, density: 100/108},
+});
+
 fetch("https://docs.google.com/document/export?format=txt&id=18t_9MHZTENbmYdezAAj4LRM0-Eak_MYO1HssZW2FX1U")
   .then(r => r.text())
   .then(a => {
@@ -23,7 +44,7 @@ fetch("https://docs.google.com/document/export?format=txt&id=18t_9MHZTENbmYdezAA
                           year = year_str.match(/\d{2}/) * 100;
                         let title = year ? line.slice(line.indexOf(':') + 2, line.lastIndexOf('[') - 1) : line.slice(line.indexOf(':') + 2);
                         let comp = line.slice(2, line.indexOf(':'));
-                        tiers.push({str: title,
+                        works.push({str: title,
                                        year: year,
                                        composer: comp,
                                        tier: i,
@@ -41,11 +62,11 @@ fetch("https://docs.google.com/document/export?format=txt&id=18t_9MHZTENbmYdezAA
                document.getElementById("rng")
                 .addEventListener("submit", function(event) {
                   event.preventDefault();
-                  let filtered = tiers.filter(i => {
-                    let begin = document.getElementById("begin").value;
-                    let end = document.getElementById("end").value;
-                    let lowest = document.getElementById("lowest").value;
-                    let highest = document.getElementById("highest").value;
+                  let filtered = works.filter(i => {
+                    let begin = dates.noUiSlider.get()[0];
+                    let end = dates.noUiSlider.get()[1];
+                    let lowest = tiers.noUiSlider.get()[0];
+                    let highest = tiers.noUiSlider.get()[1];
                     if ((begin && i.year < begin) || (end && i.year > end) || (lowest && i.tier + 1 < lowest) || (highest && i.tier + 1 > highest))
                       return false;
                     return true;
