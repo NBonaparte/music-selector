c71a1c0c5507caedcf027ccfe584ca9f0ef02ac5
NBonaparte <nbonaparte@protonmail.com>
2020-04-11 00:59:49 -0700

    feat: add live work count

--- script.js
+++ script.js
@@ -6,10 +6,18 @@ var createSlider = (id, start, step, values, density) => {
 }
 var dispElem = (id, disp) => document.getElementById(id).style.display = disp ? "block" : "none";
 var setText = (id, text) => document.getElementById(id).textContent = text;
-
 var works = [];
+var filtered = [];
 var dates = createSlider("date-range", [1000, 2020], 20, [...Array(11).keys()].map(x => (x + 10) * 100), 20/1020 * 100);
 var tiers = createSlider("tier-range", [1, 108], 1, [1, ...[...Array(11).keys()].map(x => (x + 1) * 10), 108], 100/108);
+var filterWorks = (date_range, tier_range) => {
+	filtered = works.filter(i => {
+		let [begin, end] = date_range;
+		let [lowest, highest] = tier_range;
+		return !((begin != 1000 || end != 2020) && ((i.year < begin) || (i.year > end)) || (i.tier + 1 < lowest) || (i.tier + 1 > highest));
+	});
+	document.getElementById("count").textContent = filtered.length;
+};
 
 fetch("https://docs.google.com/document/export?format=txt&id=18t_9MHZTENbmYdezAAj4LRM0-Eak_MYO1HssZW2FX1U")
 	.then(r => r.text())
@@ -50,22 +58,20 @@ fetch("https://docs.google.com/document/export?format=txt&id=18t_9MHZTENbmYdezAA
 										}
 									}
 								}
+								filterWorks(dates.noUiSlider.get(), tiers.noUiSlider.get());
 								dispElem("rng", true);
 								dispElem("info", false);
+								dates.noUiSlider.on("set", new_dates => filterWorks(new_dates, tiers.noUiSlider.get()));
+								tiers.noUiSlider.on("set", new_tiers => filterWorks(dates.noUiSlider.get(), new_tiers));
 								document.getElementById("rng").addEventListener("submit", function(event) {
 									event.preventDefault();
-									let filtered = works.filter(i => {
-										let [begin, end] = dates.noUiSlider.get();
-										let [lowest, highest] = tiers.noUiSlider.get();
-										return !((begin != 1000 || end != 2020) && ((i.year < begin) || (i.year > end)) || (i.tier + 1 < lowest) || (i.tier + 1 > highest));
-									});
 									let piece = filtered[randInt(0, filtered.length - 1)];
 									if (piece) {
 										dispElem("info", false);
 										setText("composer", piece.comp);
 										setText("title", smartquotes(piece.title));
 										document.getElementById("yt").href = "https://www.youtube.com/results?search_query=" + encodeURIComponent(piece.comp + " " + piece.title);
-										setText("tier", "Tier " + piece.tier);
+										setText("tier", piece.tier);
 										setText("year", piece.year ? "(" + piece.year + ")" : "");
 										dispElem("result", true);
 									} else {
